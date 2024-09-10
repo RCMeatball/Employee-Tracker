d930086b1dac7ef683e0b3b5351294a65cd9768e
Rylan Coglaiti <rylan.coglaiti@gmail.com>
2024-09-10 19:27:47 -0400

    still more js

--- index.js
+++ index.js
@@ -97,3 +97,101 @@ const viewDepartments = () => {
     employees_db();
   });
 };
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "firstName",
+          message: "Enter employee first name",
+        },
+        {
+          type: "input",
+          name: "lastName",
+          message: "Enter employee last name",
+        },
+        {
+          type: "input",
+          name: "roleId",
+          message: "Enter employee role ID",
+        },
+        {
+          type: "input",
+          name: "managerId",
+          message: "Enter employee manager ID",
+        },
+      ])
+      .then((answers) => {
+        const { firstName, lastName, roleId, managerId } = answers;
+        pool.query('INSERT INTO employee (first_name, last_name, role_Id, manager_Id) VALUES'
+            [firstName, lastName, roleId, managerId],
+            (err, res) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log(`Employee added: ${firstName} ${lastName}`);
+                employees_db();
+            }
+        );
+      });
+    };
+
+    const addRole = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "roleTitle",
+                message: "Enter new role title",
+            },
+            {
+                type: "input",
+                name: "roleDepartment",
+                message: "Enter new role department",
+            },
+            {
+                type: "input",
+                name: "roleSalary",
+                message: "Enter new role salary",
+            }
+        ])
+        .then((answers) => {
+            const { roleTitle, roleDepartment, roleSalary } = answers;
+            pool.query(
+                'INSERT INTO role (title, department_id, salary) VALUES',
+                [roleTitle, roleDepartment, roleSalary],
+                (err, res) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    console.log(`Added the new role of ${roleTitle}`);
+                    employees_db();
+                }
+            );
+        });
+    };
+
+    const addDepartment = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "departmentName",
+                message: "Enter the new department name"
+            }
+        ])
+        .then((answers) => {
+            const { departmentName } = answers;
+            pool.query('INSERT INTO department (name) VALUES',
+                [departmentName],
+                (err, res) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    console.log(`Added the new department of ${departmentName}`);
+                    employees_db();
+                }
+            );
+        });
+    };
